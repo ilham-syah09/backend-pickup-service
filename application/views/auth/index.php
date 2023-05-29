ca1c6e8f806bd92060689b21d801c521c1aadd63
bastiannazaromi <bastian.nazaromi@gmail.com>
2023-05-29 08:49:20 +0700

    remove register

--- application/views/auth/index.php
+++ application/views/auth/index.php
@@ -54,21 +54,10 @@
                                             please fill in your password
                                         </div>
                                     </div>
-
-                                    <div class="form-group">
-                                        <div class="custom-control custom-checkbox">
-                                            <input type="checkbox" name="remember" class="custom-control-input" tabindex="3" id="remember-me">
-                                            <label class="custom-control-label" for="remember-me">Remember Me</label>
-                                        </div>
-                                    </div>
-
                                     <div class="form-group">
                                         <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
                                             Login
                                         </button>
-                                    </div>
-                                    <div class="text-center">
-                                        <span>Belum punya akun?</span><a href="<?= base_url('registrasi'); ?>"> Daftar disini</a> <br>
                                     </div>
                                 </form>
                             </div>
