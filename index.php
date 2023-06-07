ff95738ea062e94363fccfd63cba35aafe17a3d8
bastiannazaromi <bastian.nazaromi@gmail.com>
2023-06-07 10:39:35 +0700

    index

--- index.php
+++ index.php
@@ -59,7 +59,7 @@ if (!defined('ENVIRONMENT')) {
 	$domain = strtolower($_SERVER['HTTP_HOST']);
 
 	switch ($domain) {
-		case 'https://pickupservice.sewadomain.my.id/':
+		case 'pickupservice.sewadomian.my.id':
 			define('ENVIRONMENT', 'production');
 			break;
 		default:
